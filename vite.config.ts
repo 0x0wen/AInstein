7ade957128649354a18f862f37a76040d26236ba
Maulvi Maulana <maulvizm@MacBook-Pro-Maulvi.local>
2025-03-08 22:48:01 +0700

    feat: add shadcn ui

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,20 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
-import tailwindcss from "@tailwindcss/vite";
+// biome-ignore lint/style/useNodejsImportProtocol: <explanation>
+import path from 'path';
+import tailwindcss from '@tailwindcss/vite';
+import { TanStackRouterVite } from '@tanstack/router-plugin/vite';
+import react from '@vitejs/plugin-react-swc';
+import { defineConfig } from 'vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-    TanStackRouterVite({ target: "react", autoCodeSplitting: true }),
-    react(),
-  ],
+	plugins: [
+		tailwindcss(),
+		TanStackRouterVite({ target: 'react', autoCodeSplitting: true }),
+		react(),
+	],
+	resolve: {
+		alias: {
+			'@': path.resolve(__dirname, './src'),
+		},
+	},
 });
