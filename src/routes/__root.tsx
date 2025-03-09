28dda883840ce63e5176ceb836b8ea5799af05be
owen <owentobias21@gmail.com>
2025-03-09 20:25:06 +0700

    fix: some ui errors

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { Outlet, createRootRoute } from '@tanstack/react-router';
 import { Sidebar } from "@/components/layout/sidebar"
-// import { ThemeProvider } from "@/components/theme-provider"
+import { ThemeProvider } from "@/components/theme/ThemeProvider"
 export const Route = createRootRoute({
 	component: RootComponent,
 });
@@ -10,15 +10,15 @@ function RootComponent() {
 	return (
 		<React.Fragment>
 			<body>
-        {/* <ThemeProvider attribute="class" defaultTheme="light" enableSystem> */}
-          <div className="flex flex-col min-h-screen">
+      <ThemeProvider  defaultTheme="dark" storageKey="vite-ui-theme" >
+      <div className="flex flex-col min-h-screen">
             <div className="flex flex-1">
               <Sidebar />
-              <main className="flex-1">			<Outlet />
+              <main className="flex-1 bg-white">			<Outlet />
 			  </main>
             </div>
           </div>
-        {/* </ThemeProvider> */}
+        </ThemeProvider>
       </body>
 		</React.Fragment>
 	);
